4548bdb59787ee012946b53ec82d07ecc66e9ee3
rao <rao826699158@gmail.com>
2025-07-13 21:53:29 +0800

    0.0.1第二次提交代码

--- src/main/java/com/rao/mock_server/controller/DemoController.java
+++ src/main/java/com/rao/mock_server/controller/DemoController.java
@@ -4,6 +4,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Objects;
+
 @RestController
 @RequestMapping("/demo")
 public class DemoController {
@@ -11,5 +13,18 @@ public class DemoController {
     @GetMapping("one")
     public String one(){
         return "one";
+    }
+
+    @GetMapping("two")
+    public String two(String input ){
+        if(Objects.equals(input, "1")){
+            return "1";
+        }
+        else if (Objects.equals(input,"2")){
+            return "2";
+        }
+        else {
+            return "demo";
+        }
     }
 }
